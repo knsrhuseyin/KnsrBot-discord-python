c711b84820bdb3f6966b6ccd07370de7dbab00d8
knsrhuseyin <huseyin.kenesari2@gmail.com>
2023-02-08 22:13:37 +0100

    Fichier principal

--- main.py
+++ main.py
@@ -1,3 +1,20 @@
+import discord
+from discord import *
 
-if __name__ == '__main__':
-    print("Hello World")
+
+class MyClient(discord.Client):
+    async def on_ready(self):
+        print('Logged in as')
+        print(self.user.name)
+        print(self.user.id)
+        print('------')
+        game = discord.Game("Test")
+        await client.change_presence(status=discord.Status.idle, activity=game)
+
+
+intents = discord.Intents.default()
+intents.message_content = True
+
+
+client = MyClient(intents=intents)
+client.run('ODE3MTc2OTc5MzIwNzMzNzM0.GyjLn6.hOuJ-EAdSD2_8ifdMVBRznye715BjiObpkj6LM')
